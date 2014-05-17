0f949b6965bda5a7b7dc5e1905770044f418da81
gogotanaka <qlli.illb@gmail.com>
2014-05-17 15:37:41 +0900

    Improve users#

--- lib/scraper/service.rb
+++ lib/scraper/service.rb
@@ -1,9 +1,7 @@
 module Scraper
-  # サービス指向アーキテクチャ
-  # http://ja.wikipedia.org/wiki/%E3%82%B5%E3%83%BC%E3%83%93%E3%82%B9%E6%8C%87%E5%90%91%E3%82%A2%E3%83%BC%E3%82%AD%E3%83%86%E3%82%AF%E3%83%81%E3%83%A3
-
-  attr_accessor :urls
   class Service
+    attr_accessor :urls
+
     def initialize(urls)
       @urls = urls
     end
