dad913f0954c84d84a14996fc3276ff647a8625c
gogotanaka <qlli.illb@gmail.com>
2014-05-23 03:36:12 +0900

    Fix videos#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
     end
 
     def user_params
-      params.require(:user).permit(:contents, :name, :image)
+      params.require(:user).permit(:contents, :name, :image, :email)
     end
 
     def authenticate_user
